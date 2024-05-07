7ab85b649d4dd753776f044bdb72faecd70c7299
Greg Smith <gregorymichaelsmith@protonmail.com>
2024-05-06 17:12:22 -0700

    Updated database insertion script

--- input_data/data-insertion.sql
+++ input_data/data-insertion.sql
@@ -8,4 +8,7 @@ VALUES ('Computer Science', 'CSCI'),
 
 INSERT INTO course (name, number, department)
 VALUES ('Foundations of Computing', 241, 1),
-       ('Elements of Statistics', 250, 2);
+       ('Elements of Statistics', 250, 2),
+       ('Computer Science I', 121, 1),
+       ('Computer Science II', 221, 1),
+       ('Analytic Geometry and Calculus I', 234, 2);
